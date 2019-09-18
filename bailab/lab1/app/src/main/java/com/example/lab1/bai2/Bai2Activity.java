c39098c9b6cac8df944b9c589a71524c228a6a55
Ky Thuat 94TV <ngockhac01012000@gmail.com>
2019-09-19 01:00:25 +0700

    lab

---
+++ bailab/lab1/app/src/main/java/com/example/lab1/bai2/Bai2Activity.java
@@ -0,0 +1,23 @@
+package com.example.lab1.bai2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.lab1.R;
+
+public class Bai2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2);
+    }
+
+    public void startService(View view) {
+    }
+
+    public void stopService(View view) {
+    }
+}
--- bailab/lab1/app/src/main/java/com/example/lab1/bai3/Bai3Activity.java
+++ bailab/lab1/app/src/main/java/com/example/lab1/bai3/Bai3Activity.java
@@ -3,14 +3,78 @@ package com.example.lab1.bai3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.lab1.R;
 
 public class Bai3Activity extends AppCompatActivity {
+    private EditText edt_nhap;
+    private Button btn_timvana, btn_timvanb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bai3);
+        initview();
+        handling();
     }
+
+    private void handling() {
+        btn_timvana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = edt_nhap.getText().toString();
+                if (text.trim().equals("")) {
+                    Toast.makeText(Bai3Activity.this, "không được để trống ", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    char kyTu = 'a';
+                    int count = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        if (text.charAt(i) == kyTu) {
+                            count++;
+                        }
+                    }
+                    Toast.makeText(Bai3Activity.this,
+                            "Số lần xuất hiện của ký tự  " + kyTu +
+                                    " trong chuỗi " + text + " = " + count,
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+        btn_timvanb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = edt_nhap.getText().toString();
+                if (text.trim().equals("")) {
+                    Toast.makeText(Bai3Activity.this, "không được để trống ", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    char kyTu = 'b';
+                    int count = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        if (text.charAt(i) == kyTu) {
+                            count++;
+                        }
+                    }
+                    Toast.makeText(Bai3Activity.this,
+                            "Số lần xuất hiện của ký tự  " + kyTu +
+                                    " trong chuỗi " + text + " = " + count,
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+    private void initview() {
+        edt_nhap = findViewById(R.id.edt_nhap);
+        btn_timvana = findViewById(R.id.btn_timvana);
+        btn_timvanb = findViewById(R.id.btn_timvanb);
+    }
+
 }
