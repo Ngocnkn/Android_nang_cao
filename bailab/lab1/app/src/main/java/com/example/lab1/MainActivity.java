c0bde4a3dd761e4770abd3af9e2ebd1e85773738
Ky Thuat 94TV <ngockhac01012000@gmail.com>
2019-09-19 00:16:51 +0700

    up

--- bailab/lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ bailab/lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bai1(View view) {
+    }
+
+    public void bai2(View view) {
+    }
+
+    public void bai3(View view) {
+    }
+
+    public void bai4(View view) {
     }
 }
