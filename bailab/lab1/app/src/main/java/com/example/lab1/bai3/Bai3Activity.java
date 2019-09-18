635c711e4bb4dcac79d4dacecb54f5219879b18c
Ky Thuat 94TV <ngockhac01012000@gmail.com>
2019-09-19 00:20:33 +0700

    lab

---
+++ bailab/lab1/app/src/main/java/com/example/lab1/bai3/Bai3Activity.java
@@ -0,0 +1,16 @@
+package com.example.lab1.bai3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab1.R;
+
+public class Bai3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai3);
+    }
+}
